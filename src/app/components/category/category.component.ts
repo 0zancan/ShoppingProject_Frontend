372bad453b02dff9035e72811d0a2992de38f5cc
Ozan Can <48280981+0zancan@users.noreply.github.com>
2021-09-27 17:40:36 +0300

    added cart

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/models/category';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+  styleUrls: ['./category.component.css'],
 })
 export class CategoryComponent implements OnInit {
+  categories: Category[] = [];
+  currentCategory: Category;
 
-  constructor() { }
+  constructor(private categoryService: CategoryService) {}
 
   ngOnInit(): void {
+    this.getCategories();
   }
 
+  getCategories() {
+    this.categoryService.getCategories().subscribe((response) => {
+      this.categories = response.data;
+    });
+  }
+
+  setCurrentCategory(category: Category) {
+    this.currentCategory = category;
+  }
+
+  clearCurrentCategory(){
+    return this.currentCategory;
+  }
+
+  getCurrentCategoryClass(category: Category) {
+    if (this.currentCategory == category) {
+      return 'list-group-item active';
+    } else {
+      return 'list-group-item';
+    }
+  }
+
+  getAllCategoryClass() {
+    if (!this.currentCategory) {
+      return 'list-group-item active';
+    } else {
+      return 'list-group-item';
+    }
+  }
 }
